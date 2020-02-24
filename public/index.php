7d1c67254e0033f7179f420c9f4eaafa838ddda0
mcdanis <mcdanisaputra@gmail.com>
2020-02-24 16:08:48 +0700

    restore
    restore kembali seperti semula

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
-// ini ketiga
+ 
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-// halllo
+ 
 define('LARAVEL_START', microtime(true));
 
 /*
